3570db4c8db6be570c4f0dcaf0a3465331078181
fabricio <fpanamamendez@gmail.com>
2023-11-16 11:21:05 -0400

    nuevo

--- back/server.js
+++ back/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
 
-const app =express();
+const app = express();
 
 app.use(express.json());
 
@@ -14,18 +14,49 @@ const db = mysql.createConnection({
     database: "analisis2"
 });
 
+// Ruta para el registro de nuevos usuarios
+app.post('/register', (req, res) => {
+    const { email, password, firstName, lastName } = req.body;
 
-app.post('/login',(req, res)=>{
-    const sql = "SELECT * FROM clientes WHERE email = ? AND psw = ?";
-    db.query(sql,[req.body.email, req.body.password] , (err,data) => {
-        if(err) return res.json("noo :(");
-        if(data.length > 0){
-            return res.json("logeado");
-        }else{
-            return res.json("no logeado");
+    // Verifica si el usuario ya existe en la base de datos
+    const checkUserQuery = "SELECT * FROM clientes WHERE email = ?";
+    db.query(checkUserQuery, [email], (checkErr, checkData) => {
+        if (checkErr) {
+            return res.json({ error: true, message: "Error al verificar el usuario" });
         }
-    })
-})
-app.listen(8081,()=>{
-    console.log("siii..")
-})
+
+        if (checkData.length > 0) {
+            return res.json({ error: true, message: "El usuario ya existe" });
+        }
+
+        // Inserta un nuevo usuario con nombre y apellido
+        const insertUserQuery = "INSERT INTO clientes (email, psw, nombre, apellido) VALUES (?, ?, ?, ?)";
+        db.query(insertUserQuery, [email, password, firstName, lastName], (insertErr) => {
+            if (insertErr) {
+                return res.json({ error: true, message: "Error al registrar el usuario" });
+            }
+
+            return res.json("Registrado exitosamente");
+        });
+    });
+});
+
+// Ruta para el inicio de sesión
+app.post('/login', (req, res) => {
+    const sql = "SELECT * FROM clientes WHERE email = ? AND psw = ?";
+    db.query(sql, [req.body.email, req.body.password], (err, data) => {
+        if (err) {
+            return res.json({ error: true, message: "Error en la consulta" });
+        }
+
+        if (data.length > 0) {
+            return res.json("logeado");
+        } else {
+            return res.json({ error: true, message: "Credenciales incorrectas" });
+        }
+    });
+});
+
+app.listen(8081, () => {
+    console.log("Servidor iniciado en el puerto 8081");
+});
